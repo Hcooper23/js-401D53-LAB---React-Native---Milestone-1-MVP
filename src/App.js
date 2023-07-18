70927ede350e6b14121a1074d0fe9cf13d27d2e3
Hayden James Cooper <haydencpr95@gmail.com>
2023-07-17 22:41:39 -0400

    Functining

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import App from '../App';
+
+export default function Main() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <App />
+    </>
+  );
+}
